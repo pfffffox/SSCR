886eac89bf804641c2ae23347db62aea01a7428a
pfffffox <43240523+pfffffox@users.noreply.github.com>
2018-09-23 16:15:22 +0300

    Update SimpleDraw.h

--- Source/SimpleDraw.h
+++ Source/SimpleDraw.h
@@ -4,21 +4,21 @@ BOOL Box(HDC hdc, float x1, float y1, int size);
 
 BOOL Line(HDC hdc, int x1, int y1, float x2, float y2)
 {
-	MoveToEx(hdc, x1, y1, NULL); //сделать текущими координаты x1, y1
-	return LineTo(hdc, (int)x2, (int)y2); //нарисовать линию
+	MoveToEx(hdc, x1, y1, NULL); //Move point to
+	return LineTo(hdc, (int)x2, (int)y2); //draw line
 }
 
 BOOL Box(HDC hdc, float x1, float y1, int size)
 {
-	MoveToEx(hdc, (int)x1 + size, (int)y1 + size, NULL);
-	LineTo(hdc, (int)x1 + size, (int)y1 - size);
-	LineTo(hdc, (int)x1 - size, (int)y1 - size);
+	MoveToEx(hdc, (int)x1 + size, (int)y1 + size, NULL); //move poitn to begin draw
+	LineTo(hdc, (int)x1 + size, (int)y1 - size); 
+	LineTo(hdc, (int)x1 - size, (int)y1 - size);  
 	LineTo(hdc, (int)x1 - size, (int)y1 + size);
-	LineTo(hdc, (int)x1 + size, (int)y1 + size);
+	LineTo(hdc, (int)x1 + size, (int)y1 + size); // drawing sides
 	return 0;
 }
 
-BOOL Rotate(HDC hDC,float rotate_buf, float draw_x, float draw_y)
+BOOL Rotate(HDC hDC,float rotate_buf, float draw_x, float draw_y) //shit rotate but better then nothing
 {
 if (22.5 < rotate_buf & rotate_buf < 67.5)
 	Line(hDC, draw_x, draw_y, draw_x - 75, draw_y - 75);
